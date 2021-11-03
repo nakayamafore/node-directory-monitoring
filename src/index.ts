f7d069d9a5050601d4337f3de8a2690f0084f4d3
kouichi nakayama <mable.index.matrix@gmail.com>
2021-11-03 18:16:20 +0900

    OCR結果をwebSocketで配信

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,42 @@
 'use strict'
-const chokidar = require('chokidar')
+import chokidar from 'chokidar'
 import { createWorker } from 'tesseract.js'
-
+import io from 'socket.io-client'
+const socket = io('http://localhost:7000')
 const watcher = chokidar.watch('./', {
   ignored: /[\/\\]\./,
   persistent: true,
 })
-const worker = createWorker({
-  logger: (m: any) => console.log(m),
-})
+
 //イベント定義
-watcher.on('ready', async () => {
-  //スタンバイ状態
-  console.log('ready watching...')
+socket.on('connect', () => {
+  console.log(`connect`)
+  watcher.on('ready', async () => {
+    //スタンバイ状態
+    console.log('ready watching...')
 
-  //ファイルを追加したとき
-  watcher.on('add', async (path: string) => {
-    console.log(path + ' added.')
-    await worker.load()
-    await worker.loadLanguage('eng')
-    await worker.initialize('eng')
-    const {
-      data: { text },
-    } = await worker.recognize(path)
-    console.log(text)
-  })
+    //ファイルを追加したとき
+    watcher.on('add', async (path: string) => {
+      console.log('added: ' + path)
+      const worker = createWorker({
+        logger: (m: any) => {},
+      })
+      await worker.load()
+      await worker.loadLanguage('eng')
+      await worker.initialize('eng')
+      const {
+        data: { text },
+      } = await worker.recognize(path)
 
-  //ファイルの編集
-  watcher.on('change', (path: string) => {
-    console.log(path + ' changed.')
+      // 不要な改行を削除
+      const trimed = text.replace(/([^\.;:>!{])\r?\n/g, '$1')
+      console.log(' socket.emit: ' + path)
+      socket.emit('message', trimed)
+    })
+
+    //ファイルの編集
+    watcher.on('change', (path: string) => {
+      // console.log(path + ' changed.')
+    })
   })
 })
